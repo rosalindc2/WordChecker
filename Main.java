37c17ab1131d16f1965edf85726d3991e4b43f24
rosalindc2 <rosalindc2@nycstudents.net>
2024-10-09 13:10:00 +0000

    1

--- Main.java
+++ Main.java
@@ -1,4 +1,8 @@
-public class Main
-{ 
-    public static void main (String[] args)
-    { 
+import java.util.ArrayList;
+
+public static void main(String[] args)
+    {
+        WordChecker w = new WordChecker();
+        ArrayList<String> words = new ArrayList<String>();
+        WordChecker x = new WordChecker(words);
+    }
