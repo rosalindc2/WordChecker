748a43dddd09856783900d38a9bec9a6e9e3daba
rosalindc2 <rosalindc2@nycstudents.net>
2024-10-24 13:32:51 +0000

    5

--- Main.java
+++ Main.java
@@ -3,13 +3,14 @@ public class Main
 { 
     public static void main(String[] args)
     {
-        ArrayList<String> words = new ArrayList<>();
+        ArrayList<String> words = new ArrayList<String>();
         words.add("an");
         words.add("band");
         words.add("band");
         words.add("abandon");
+       // WordChecker w = new WordChecker(words);
         System.out.println(words);
-
+       WordChecker x = new WordChecker(words);
         System.out.println(x.isWordChain());
     }
 }
--- WordChecker.java
+++ WordChecker.java
@@ -1,25 +1,25 @@
 import java.util.ArrayList;
 
-public class WordChecker 
+public class WordChecker
 {
     private ArrayList <String> wordList; 
     public WordChecker(ArrayList<String> list)
     {
         wordList = new ArrayList<String>();
     }
-    Wordchecker w = new WordChecker(words);
-}
     public boolean isWordChain() 
     {
-        for(int i=1; i , wordList.size(); i++)
+        for(int i = 1; i < wordList.size(); i++)
+        {
         String after = wordList.get(i);
-        String before = wordList.get(i-1);
-        if(after.indexOf(before)<0)return false;
+        String before = wordList.get(i - 1);
+        if(after.indexOf(before) < 0)return false;
+        }
+        return true;
     }
 
     public ArrayList<String> createList(String target) 
     {
         return new ArrayList;
     }
-
 }
